94f26815cecc4d15d70426cac78054570a8692b6
Grant Keith <grant.keith@mail.mcgill.ca>
2020-03-11 18:21:37 -0400

    added scoreboard

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+var player1Wins=0, player2Wins=0, ties=0;
+
 function main(){
     var dice1 = randomDice(1);
     var dice2 = randomDice(2);
@@ -9,14 +11,20 @@ function main(){
     if(diceNum1>diceNum2){
         document.querySelector("h1").innerHTML = "Player 1 Wins!";
         console.log(document.querySelector("h1").innerHTML);
+        player1Wins++;
+        document.getElementById("player1Wins").innerHTML = player1Wins;
     }
     else if(diceNum1<diceNum2){
         document.querySelector("h1").innerHTML = "Player 2 Wins!";
         console.log(document.querySelector("h1").innerHTML);
+        player2Wins++;
+        document.getElementById("player2Wins").innerHTML = player2Wins;
     }
     else{
         document.querySelector("h1").innerHTML = "It's a Tie!";
         console.log(document.querySelector("h1").innerHTML);
+        ties++;
+        document.getElementById("ties").innerHTML = ties;
     }
 }
 
