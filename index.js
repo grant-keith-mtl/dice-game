a9551ac781d07cb7d75e1b3f444f5e08abb19fa1
Grant Keith <grant.keith@mail.mcgill.ca>
2020-03-11 18:05:52 -0400

    added reroll button

--- index.js
+++ index.js
@@ -1,43 +1,22 @@
-function main(imgNum){
-    randomDice(imgNum);
-    var srcString1 = document.getElementsByClassName("img" + imgNum)[0].getAttribute("src");
+function main(){
+    var dice1 = randomDice(1);
+    var dice2 = randomDice(2);
+    var srcString1 = document.getElementsByClassName("img1")[0].getAttribute("src");
     var diceNum1 = parseInt(srcString1.charAt(11));
+    var srcString2 = document.getElementsByClassName("img2")[0].getAttribute("src");
+    var diceNum2 = parseInt(srcString2.charAt(11));
     console.log(diceNum1);
-    var srcString2;
-    var diceNum2;
-    if(imgNum==1){
-        srcString2 = document.getElementsByClassName("img2")[0].getAttribute("src");
-        diceNum2 = parseInt(srcString2.charAt(11));
-        console.log(diceNum2);
-        if(diceNum1>diceNum2){
-            document.querySelector("h1").innerHTML = "Player 1 Wins!";
-            console.log(document.querySelector("h1").innerHTML);
-        }
-        else if(diceNum1<diceNum2){
-            document.querySelector("h1").innerHTML = "Player 2 Wins!";
-            console.log(document.querySelector("h1").innerHTML);
-        }
-        else{
-            document.querySelector("h1").innerHTML = "It's a Tie!";
-            console.log(document.querySelector("h1").innerHTML);
-        }
+    if(diceNum1>diceNum2){
+        document.querySelector("h1").innerHTML = "Player 1 Wins!";
+        console.log(document.querySelector("h1").innerHTML);
     }
-    if(imgNum==2){
-        srcString2 = document.getElementsByClassName("img1")[0].getAttribute("src");
-        diceNum2 = parseInt(srcString2.charAt(11));
-        console.log(diceNum2);
-        if(diceNum2>diceNum1){
-            document.querySelector("h1").innerHTML = "Player 1 Wins!";
-            console.log(document.querySelector("h1").innerHTML);
-        }
-        else if(diceNum2<diceNum1){
-            document.querySelector("h1").innerHTML = "Player 2 Wins!";
-            console.log(document.querySelector("h1").innerHTML);
-        }
-        else{
-            document.querySelector("h1").innerHTML = "It's a Tie!";
-            console.log(document.querySelector("h1").innerHTML);
-        }
+    else if(diceNum1<diceNum2){
+        document.querySelector("h1").innerHTML = "Player 2 Wins!";
+        console.log(document.querySelector("h1").innerHTML);
+    }
+    else{
+        document.querySelector("h1").innerHTML = "It's a Tie!";
+        console.log(document.querySelector("h1").innerHTML);
     }
 }
 
